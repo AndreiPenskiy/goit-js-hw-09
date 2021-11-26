81a58725db5380f326e47ec8ad986ec5ae9e6124
Andrei <andreipenskiy@gmail.com>
2021-11-26 13:16:44 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener("submit", btnClick);
+
+function btnClick (e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.currentTarget;
+   
+  let delayPromise = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, delayPromise)
+       .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        }, delay)
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+        }, delay)
+      });
+    delayPromise += Number(step.value);
+  };
+  
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay })
+    } else {
+      reject({ position, delay })
+    }
+  });
+};
